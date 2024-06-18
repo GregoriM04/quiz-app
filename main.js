d8350069235aaba7a635513a3c612459ef59bdbb
Gregori Martinez <gregorim143@gmail.com>
2024-06-18 19:45:58 -0400

    main.js file  declutter

--- main.js
+++ main.js
@@ -1,58 +1,4 @@
-//  Quiz questions
-// const generalQuestions = [
-//   {
-//     question:
-//       "In the &quot;Call Of Duty: Zombies&quot; map &quot;Moon&quot;, there is a secondary called the QED. What does QED stand for?",
-//     answers: [
-//       { option: "Portuguese", correct: false },
-//       { option: "Spanish", correct: false },
-//       { option: "English", correct: true },
-//       { option: "Chinese", correct: false },
-//       { option: "French", correct: false },
-//     ],
-//   },
-//   {
-//     question: "Which is the largest country in America?",
-//     answers: [
-//       { option: "Russia", correct: false },
-//       { option: "United States", correct: false },
-//       { option: "Mexico", correct: false },
-//       { option: "Brazil", correct: false },
-//       { option: "Canada", correct: true },
-//     ],
-//   },
-//   {
-//     question: "Which is the lasgest animal in the world?",
-//     answers: [
-//       { option: "Shark", correct: false },
-//       { option: "Elephant", correct: false },
-//       { option: "Giraffe", correct: false },
-//       { option: "Blue whale", correct: true },
-//       { option: "Rino", correct: false },
-//     ],
-//   },
-//   {
-//     question: "Which is the smallest coutry in the world?",
-//     answers: [
-//       { option: "Trinidad and Tobago", correct: false },
-//       { option: "Puerto Rico", correct: false },
-//       { option: "Vatican City", correct: true },
-//       { option: "Austria", correct: false },
-//       { option: "Nepal", correct: false },
-//     ],
-//   },
-//   {
-//     question: "Romania belongs to: ",
-//     answers: [
-//       { option: "America", correct: false },
-//       { option: "Oceania", correct: false },
-//       { option: "Africa", correct: false },
-//       { option: "Asia", correct: false },
-//       { option: "Europe", correct: true },
-//     ],
-//   },
-// ];
-
+//  Quiz - main functionality
 const questionElement = document.getElementById("question");
 const answerButtons = document.getElementById("answer-btn");
 const nextButton = document.getElementById("next-btn");
@@ -70,13 +16,12 @@ const questionsFetched = [];
     );
 
     if (!response.ok) {
-      throw new Error("Ops! Something went wrong with the fetch");
+      questionElement.innerHTML = "Ops! Something went wrong. Reload the page!";
     }
 
     const data = await response.json();
 
-    // console.log(data.results[9]);
-
+    // load data in once global array
     data.results.forEach((result) => {
       questionsFetched.push(result);
     });
@@ -111,36 +56,31 @@ function showQuestion() {
   allAnswers = otherAnswers.slice();
   allAnswers.push(correctAnswer);
 
-//   console.log(questionsFetched);
-
-
   allAnswers.forEach((answer) => {
     const button = document.createElement("button");
     button.innerHTML = answer;
     button.classList.add("btn");
     answerButtons.appendChild(button);
 
-    // console.log(button.innerText);
-
     // clicky event
     button.addEventListener("click", (e) => {
-        const selectedBtn = e.target;
+      const selectedBtn = e.target;
 
-        if(selectedBtn.innerText === correctAnswer) {
-            selectedBtn.classList.add("correct");
-            score++;
-        } else {
-            selectedBtn.classList.add("incorrect");
+      if (selectedBtn.innerText === correctAnswer) {
+        selectedBtn.classList.add("correct");
+        score++;
+      } else {
+        selectedBtn.classList.add("incorrect");
+      }
+
+      // look and display the correct answer when wrong is selected
+      Array.from(answerButtons.children).forEach((button) => {
+        if (button.innerText === correctAnswer) {
+          button.classList.add("correct");
         }
-
-        // look and display the correct answer
-        Array.from(answerButtons.children).forEach((button) => {
-            if (button.innerText === correctAnswer) {
-                button.classList.add("correct");
-            }
-            button.disabled = true;
-        });
-        nextButton.style.display = "flex";
+        button.disabled = true;
+      });
+      nextButton.style.display = "flex";
     });
   });
 }
@@ -151,26 +91,6 @@ function resetState() {
     answerButtons.removeChild(answerButtons.firstChild);
   }
 }
-
-// function selectAnswer(e) {
-//   const selectedBtn = e.target;
-//   const isCorrect = selectedBtn.dataset.correct === "true";
-//   if (isCorrect) {
-//     selectedBtn.classList.add("correct");
-//     score++;
-//   } else {
-//     selectedBtn.classList.add("incorrect");
-//   }
-
-//   // look and display correct option
-//   Array.from(answerButtons.children).forEach((button) => {
-//     if (button.dataset.correct === "true") {
-//       button.classList.add("correct");
-//     }
-//     button.disabled = true;
-//   });
-//   nextButton.style.display = "flex";
-// }
 
 function showScore() {
   resetState();
