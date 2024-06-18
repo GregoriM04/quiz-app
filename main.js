ac7ec353abc3418f17229d8cc600987151307472
Gregori Martinez <gregorim143@gmail.com>
2024-06-18 18:31:37 -0400

    refactor main functionality with an API

--- main.js
+++ main.js
@@ -1,57 +1,57 @@
 //  Quiz questions
-const generalQuestions = [
-  {
-    question:
-      "In the &quot;Call Of Duty: Zombies&quot; map &quot;Moon&quot;, there is a secondary called the QED. What does QED stand for?",
-    answers: [
-      { option: "Portuguese", correct: false },
-      { option: "Spanish", correct: false },
-      { option: "English", correct: true },
-      { option: "Chinese", correct: false },
-      { option: "French", correct: false },
-    ],
-  },
-  {
-    question: "Which is the largest country in America?",
-    answers: [
-      { option: "Russia", correct: false },
-      { option: "United States", correct: false },
-      { option: "Mexico", correct: false },
-      { option: "Brazil", correct: false },
-      { option: "Canada", correct: true },
-    ],
-  },
-  {
-    question: "Which is the lasgest animal in the world?",
-    answers: [
-      { option: "Shark", correct: false },
-      { option: "Elephant", correct: false },
-      { option: "Giraffe", correct: false },
-      { option: "Blue whale", correct: true },
-      { option: "Rino", correct: false },
-    ],
-  },
-  {
-    question: "Which is the smallest coutry in the world?",
-    answers: [
-      { option: "Trinidad and Tobago", correct: false },
-      { option: "Puerto Rico", correct: false },
-      { option: "Vatican City", correct: true },
-      { option: "Austria", correct: false },
-      { option: "Nepal", correct: false },
-    ],
-  },
-  {
-    question: "Romania belongs to: ",
-    answers: [
-      { option: "America", correct: false },
-      { option: "Oceania", correct: false },
-      { option: "Africa", correct: false },
-      { option: "Asia", correct: false },
-      { option: "Europe", correct: true },
-    ],
-  },
-];
+// const generalQuestions = [
+//   {
+//     question:
+//       "In the &quot;Call Of Duty: Zombies&quot; map &quot;Moon&quot;, there is a secondary called the QED. What does QED stand for?",
+//     answers: [
+//       { option: "Portuguese", correct: false },
+//       { option: "Spanish", correct: false },
+//       { option: "English", correct: true },
+//       { option: "Chinese", correct: false },
+//       { option: "French", correct: false },
+//     ],
+//   },
+//   {
+//     question: "Which is the largest country in America?",
+//     answers: [
+//       { option: "Russia", correct: false },
+//       { option: "United States", correct: false },
+//       { option: "Mexico", correct: false },
+//       { option: "Brazil", correct: false },
+//       { option: "Canada", correct: true },
+//     ],
+//   },
+//   {
+//     question: "Which is the lasgest animal in the world?",
+//     answers: [
+//       { option: "Shark", correct: false },
+//       { option: "Elephant", correct: false },
+//       { option: "Giraffe", correct: false },
+//       { option: "Blue whale", correct: true },
+//       { option: "Rino", correct: false },
+//     ],
+//   },
+//   {
+//     question: "Which is the smallest coutry in the world?",
+//     answers: [
+//       { option: "Trinidad and Tobago", correct: false },
+//       { option: "Puerto Rico", correct: false },
+//       { option: "Vatican City", correct: true },
+//       { option: "Austria", correct: false },
+//       { option: "Nepal", correct: false },
+//     ],
+//   },
+//   {
+//     question: "Romania belongs to: ",
+//     answers: [
+//       { option: "America", correct: false },
+//       { option: "Oceania", correct: false },
+//       { option: "Africa", correct: false },
+//       { option: "Asia", correct: false },
+//       { option: "Europe", correct: true },
+//     ],
+//   },
+// ];
 
 const questionElement = document.getElementById("question");
 const answerButtons = document.getElementById("answer-btn");
@@ -60,6 +60,30 @@ let nextButtonIcon = document.createElement("ion-icon");
 
 let currentQuestionIndex = 0;
 let score = 0;
+
+// fetching questions and answers
+const questionsFetched = [];
+(async function () {
+  try {
+    const response = await fetch(
+      "https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple"
+    );
+
+    if (!response.ok) {
+      throw new Error("Ops! Something went wrong with the fetch");
+    }
+
+    const data = await response.json();
+
+    // console.log(data.results[9]);
+
+    data.results.forEach((result) => {
+      questionsFetched.push(result);
+    });
+  } catch (error) {
+    console.error("Error fetching data", error);
+  }
+})();
 
 function startQuiz() {
   currentQuestionIndex = 0;
@@ -74,23 +98,50 @@ function showQuestion() {
   // remove previous answer options
   resetState();
 
-  // display the current question
-  let currentQuestion = generalQuestions[currentQuestionIndex];
+  // load and display the current question
+  let currentQuestion = questionsFetched[currentQuestionIndex].question;
   let questionNumber = currentQuestionIndex + 1;
-  questionElement.innerHTML = questionNumber + ".) " + currentQuestion.question;
+  questionElement.innerHTML = questionNumber + ".) " + currentQuestion;
 
-  // display its answers
-  currentQuestion.answers.forEach((answer) => {
+  // load and display answers
+  let allAnswers = [];
+  let correctAnswer = questionsFetched[currentQuestionIndex].correct_answer;
+  let otherAnswers = questionsFetched[currentQuestionIndex].incorrect_answers;
+
+  allAnswers = otherAnswers.slice();
+  allAnswers.push(correctAnswer);
+
+//   console.log(questionsFetched);
+
+
+  allAnswers.forEach((answer) => {
     const button = document.createElement("button");
-    button.innerHTML = answer.option;
+    button.innerHTML = answer;
     button.classList.add("btn");
     answerButtons.appendChild(button);
 
+    // console.log(button.innerText);
+
     // clicky event
-    if (answer.correct) {
-      button.dataset.correct = answer.correct;
-    }
-    button.addEventListener("click", selectAnswer);
+    button.addEventListener("click", (e) => {
+        const selectedBtn = e.target;
+
+        if(selectedBtn.innerText === correctAnswer) {
+            selectedBtn.classList.add("correct");
+            score++;
+        } else {
+            selectedBtn.classList.add("incorrect");
+        }
+
+        // look and display the correct answer
+        Array.from(answerButtons.children).forEach((button) => {
+            if (button.innerText === correctAnswer) {
+                button.classList.add("correct");
+            }
+            button.disabled = true;
+        });
+        nextButton.style.display = "flex";
+    });
   });
 }
 
@@ -101,30 +152,30 @@ function resetState() {
   }
 }
 
-function selectAnswer(e) {
-  const selectedBtn = e.target;
-  const isCorrect = selectedBtn.dataset.correct === "true";
-  if (isCorrect) {
-    selectedBtn.classList.add("correct");
-    score++;
-  } else {
-    selectedBtn.classList.add("incorrect");
-  }
+// function selectAnswer(e) {
+//   const selectedBtn = e.target;
+//   const isCorrect = selectedBtn.dataset.correct === "true";
+//   if (isCorrect) {
+//     selectedBtn.classList.add("correct");
+//     score++;
+//   } else {
+//     selectedBtn.classList.add("incorrect");
+//   }
 
-  // look and display correct option
-  Array.from(answerButtons.children).forEach((button) => {
-    if (button.dataset.correct === "true") {
-      button.classList.add("correct");
-    }
-    button.disabled = true;
-  });
-  nextButton.style.display = "flex";
-}
+//   // look and display correct option
+//   Array.from(answerButtons.children).forEach((button) => {
+//     if (button.dataset.correct === "true") {
+//       button.classList.add("correct");
+//     }
+//     button.disabled = true;
+//   });
+//   nextButton.style.display = "flex";
+// }
 
 function showScore() {
   resetState();
 
-  questionElement.innerHTML = `You get ${score} out of ${generalQuestions.length}!`;
+  questionElement.innerHTML = `You get ${score} out of ${questionsFetched.length}!`;
   nextButton.innerHTML = "Play Again";
   nextButtonIcon.setAttribute("name", "play");
   nextButton.appendChild(nextButtonIcon);
@@ -133,7 +184,7 @@ function showScore() {
 
 function handleNextButton() {
   currentQuestionIndex++;
-  if (currentQuestionIndex < generalQuestions.length) {
+  if (currentQuestionIndex < questionsFetched.length) {
     showQuestion();
   } else {
     showScore();
@@ -141,11 +192,13 @@ function handleNextButton() {
 }
 
 nextButton.addEventListener("click", () => {
-  if (currentQuestionIndex < generalQuestions.length) {
+  if (currentQuestionIndex < questionsFetched.length) {
     handleNextButton();
   } else {
     startQuiz();
   }
 });
 
-startQuiz();
+setTimeout(() => {
+  startQuiz();
+}, 500);
