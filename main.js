89ccffcc2c7aeda2f933fe151463d0ded1690b18
Gregori Martinez <gregorim143@gmail.com>
2024-06-18 21:03:58 -0400

    shuffle answers function added

--- main.js
+++ main.js
@@ -68,6 +68,25 @@ function showQuestion() {
   allAnswers = otherAnswers.slice();
   allAnswers.push(correctAnswer);
 
+  // function to shuffle answers order
+  function shuffle(array) {
+    let currentIndex = array.length;
+
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+      // Pick a remaining element...
+      let randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+  }
+  shuffle(allAnswers);
+
   allAnswers.forEach((answer) => {
     const button = document.createElement("button");
     button.innerHTML = answer;
